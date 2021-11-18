6edbe3f0db61a8cc63a1a800cf9d3dbb522c6971
Manna968 <89865722+Manna968@users.noreply.github.com>
2021-11-17 21:57:39 -0500

    Update Q3.sql

--- Q3.sql
+++ Q3.sql
@@ -1,8 +1,9 @@
+use employees;
 select a.title, a.gender, avg(a.salary)
 from ( select s.emp_no, s.salary, e.gender, t.title
-    from employees.salaries s join employees.employees e
+    from salaries s join employees e
        on s.emp_no = e.emp_no
-       join employees.titles t
+       join titles t
        on s.emp_no = t.emp_no
        where s.to_date = '9999-01-01' and t.to_date = '9999-01-01'
        ) a
