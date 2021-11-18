98175dfea9df9d2e3a4b6dc69716056b52790441
Manna968 <89865722+Manna968@users.noreply.github.com>
2021-11-17 21:56:14 -0500

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -1,34 +1,35 @@
-create temporary table employees.start_employee_date
+use employees;
+create temporary table start_employee_date
 	select e.emp_no, min(t.from_date) as start_date 
-	from employees.employees e join employees.titles t on e.emp_no = t.emp_no
+	from employees e join titles t on e.emp_no = t.emp_no
 	group by e.emp_no;
 
-create temporary table employees.end_employee_date
+create temporary table end_employee_date
 	select e.emp_no, max(t.to_date) as end_date 
-	from employees.employees e join employees.titles t on e.emp_no = t.emp_no
+	from employees e join titles t on e.emp_no = t.emp_no
 	group by e.emp_no;	
 
     
-create temporary table employees.end_employee_title
+create temporary table end_employee_title
 	select e.emp_no, t.title as end_title from 
-	employees.end_employee_date e join employees.titles t on e.emp_no = t.emp_no
+	end_employee_date e join titles t on e.emp_no = t.emp_no
 	where e.end_date = t.to_date;
 
-create temporary table employees.start_employee_title
+create temporary table start_employee_title
 	select e.emp_no, t.title as start_title from 
-	employees.start_employee_date e join employees.titles t on e.emp_no = t.emp_no
+	start_employee_date e join titles t on e.emp_no = t.emp_no
 	where e.start_date = t.from_date;
 
-create temporary table employees.start_employee
+create temporary table start_employee
 	select e.emp_no, t.start_title, e.start_date from
-	employees.start_employee_date e join employees.start_employee_title t on e.emp_no = t.emp_no;
+	start_employee_date e join start_employee_title t on e.emp_no = t.emp_no;
 
-create temporary table employees.end_employee
+create temporary table end_employee
 	select e.emp_no, t.end_title, e.end_date from
-	employees.end_employee_date e join employees.end_employee_title t on e.emp_no = t.emp_no;
+	end_employee_date e join end_employee_title t on e.emp_no = t.emp_no;
 
 select a.last_name, a.first_name, b.end_title, b.end_date, b.start_title, b.start_date
-from employees.employees a join (select e.emp_no, e.end_title, e.end_date, t.start_title, t.start_date from
-employees.end_employee e join employees.start_employee t on e.emp_no = t.emp_no) b on a.emp_no = b.emp_no
+from employees a join (select e.emp_no, e.end_title, e.end_date, t.start_title, t.start_date from
+end_employee e join start_employee t on e.emp_no = t.emp_no) b on a.emp_no = b.emp_no
 order by a.last_name, a.first_name
 limit 20;
