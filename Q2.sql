95dd0483314ccb9636d636e4d9e00b3d7d140102
Manna968 <89865722+Manna968@users.noreply.github.com>
2021-11-17 21:57:13 -0500

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,14 +1,15 @@
-create temporary table employees.female_salary
+use employees;
+create temporary table female_salary
 	select avg(bb.b) as female_salary from
 	(select s.salary as b from
-	employees.employees e join employees.salaries s on e.emp_no = s.emp_no
+	employees e join salaries s on e.emp_no = s.emp_no
 	where e.gender = "F") bb; 
 
-create temporary table employees.male_salary
+create temporary table male_salary
 	select avg(aa.a) as male_salary from
 	(select s.salary as a from
-	employees.employees e join employees.salaries s on e.emp_no = s.emp_no
+	employees e join salaries s on e.emp_no = s.emp_no
 	where e.gender = "M") aa; 
     
-select a.female_salary, b.male_salary from employees.female_salary a
-, employees.male_salary b;
+select a. female_salary, b.male_salary from female_salary a
+, male_salary b;
